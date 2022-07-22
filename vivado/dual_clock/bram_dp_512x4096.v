371a4c0aa4062fe1c2a875719216eb39c013cb2f
Peter Hizalev <peter.hizalev@gmail.com>
2022-07-22 16:09:43 -0700

    Dual clock RTL and XSA

---
+++ vivado/dual_clock/bram_dp_512x4096.v
@@ -0,0 +1,30 @@
+// Dual-Port Block RAM with Two Write Ports
+// File: rams_tdp_rf_rf.v
+module bram_dp_512x4096 (clka,clkb,ena,enb,wea,web,addra,addrb,dia,dib,doa,dob);
+input clka,clkb,ena,enb,wea,web;
+input [11:0] addra,addrb;
+input [511:0] dia,dib;
+output [511:0] doa,dob;
+reg [511:0] ram [4095:0];
+reg [511:0] doa,dob;
+
+always @(posedge clka)
+begin
+  if (ena)
+    begin
+      if (wea)
+        ram[addra] <= dia;
+      doa <= ram[addra];
+    end
+end
+
+always @(posedge clkb)
+begin
+  if (enb)
+    begin
+      if (web)
+        ram[addrb] <= dib;
+      dob <= ram[addrb];
+    end
+end
+endmodule
